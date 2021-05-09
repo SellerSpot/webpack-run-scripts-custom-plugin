6bb9a4c992f01ed4e3d7e90f854ae15c18efe515
ThayalanGR <grthayalan18@gmail.com>
2021-05-09 22:13:57 +0530

    exec to spawn update and garbage process id gracefull exit implementation done @minor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Compiler } from 'webpack';
 import childProcess from 'child_process';
+import { StringDecoder } from 'string_decoder';
 
 export interface IWebpackCustomRunScriptsPluginProps {
     command?: string;
@@ -8,6 +9,8 @@ export interface IWebpackCustomRunScriptsPluginProps {
 }
 
 export class WebpackCustomRunScriptsPlugin {
+    static previousProcessId: number;
+
     private options: IWebpackCustomRunScriptsPluginProps;
 
     constructor(options: IWebpackCustomRunScriptsPluginProps) {
@@ -15,23 +18,41 @@ export class WebpackCustomRunScriptsPlugin {
     }
 
     apply(compiler: Compiler): void {
-        compiler.hooks.afterEmit.tap('Hello World Plugin', (stats) => {
+        compiler.hooks.afterEmit.tap('Webpack Custom Run Scripts Plugin', async (stats) => {
+            await this.processKillHandler(WebpackCustomRunScriptsPlugin.previousProcessId);
             const { command, allowOnWarning, allownOnError } = this.options;
             const hasException =
                 (stats.getStats().hasErrors() && !allownOnError) ||
                 (stats.getStats().hasWarnings() && !allowOnWarning);
             if (!hasException) {
-                const child = childProcess.exec(command);
-                console.log('Command Execution Started!');
+                const child = childProcess.spawn(command, {
+                    detached: true,
+                    stdio: 'pipe',
+                    shell: true,
+                });
+
+                WebpackCustomRunScriptsPlugin.previousProcessId = child.pid;
+
+                const decoder = new StringDecoder('utf8');
+
                 child.stdout.on('data', function (data) {
-                    console.log(data);
+                    const textChunk = decoder.write(data);
+                    console.log(textChunk);
                 });
                 child.stderr.on('data', function (data) {
-                    console.error(data);
+                    const textChunk = decoder.write(data);
+                    console.log(textChunk);
                 });
             } else {
                 console.error('Command Execution aborted, please fix errors and warnings!');
             }
         });
+    }
+
+    private async processKillHandler(pid: number): Promise<void> {
+        try {
+            process.kill(-pid);
+            // await this.introduceDelay();
+        } catch (error) {}
     }
 }
